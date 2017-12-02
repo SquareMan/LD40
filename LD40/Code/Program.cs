f92509cc9d909613a54c054f40945a6379808a58
Sebastian Casper <sebastiancasper3@aol.com>
2017-12-01 19:30:12 -0500

    Initial Project Setup

---
+++ LD40/Code/Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace LD40
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new LD40())
+                game.Run();
+        }
+    }
+}
